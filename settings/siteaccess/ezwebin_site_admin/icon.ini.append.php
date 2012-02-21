b2f05cc1da456dbbd533fbfc242ea8f4d9b3462b
Florian Peter <florian.peter@gmx.at>
2012-02-21 17:29:25 +0100

    Kick out unused ini settings.

--- settings/siteaccess/ezwebin_site_admin/icon.ini.append.php
+++
@@ -1,6 +0,0 @@
-<?php /* #?ini charset="utf-8"?
-
-# @bug? Setting here are not loaded--> We have to set this in siteaccess until resolved
-[ClassIcons]
-#ClassMap[hwboot_demo_folder]=filesystems/folder.png
- */ ?>
